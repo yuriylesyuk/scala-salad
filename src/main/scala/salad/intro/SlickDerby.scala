6500f439061bd94e73ea4d98daf0bcd8b63eb36f
Yuriy Lesyuk <yuriy.lesyuk@gmail.com>
2015-10-11 00:12:59 +0100

    Slick upsert statement.

--- src/main/scala/salad/intro/SlickDerby.scala
+++ src/main/scala/salad/intro/SlickDerby.scala
@@ -2,9 +2,11 @@ package salad.intro
 
 import slick.driver.DerbyDriver.api._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class UsersT (tag:Tag) extends Table[Users](tag, "USERS") {
-  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+  def id = column[Int]("ID", O.PrimaryKey)
   def name = column[String]("NAME")
   def age = column[Int]("AGE")
   def position = column[String]("POSITION")
@@ -26,4 +28,33 @@ object SlickDerby extends App{
   } finally db.close()
   
   Thread.sleep(10000)
+}
+
+object SlickUpsert extends App {
+  
+  val db = Database.forConfig("test-db")
+  
+  val users = TableQuery[UsersT]
+  
+  val actions = for {
+    all <- users.result
+  } yield all
+  
+  println( "ResultSet:")
+  val future = db.run(actions).map { _ foreach println }
+  Await.result( future, 2 seconds)
+  
+  val userMike: Users = Users(2,"Mike", 28, "Manager")
+
+  
+  val upsertNew: DBIO[Int] = users.insertOrUpdate(userMike)
+  println(
+      Await.result(db.run(upsertNew), 5 seconds)
+  )
+  
+  
+  println( "ResultSet:")
+  Await.result(db.run(users.result).map { _ foreach println }, 2 seconds)
+  
+  db.close  
 }
